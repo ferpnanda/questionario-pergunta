8051eb3e2b4c234b24b3ab9c7bb1b98484205a1a
isa <alunaisabelavaz@gmail.com>
2024-09-18 14:19:06 -0300

    first commit

---
+++ questionarioredes.py
@@ -0,0 +1,74 @@
+print('bem vindo ao questionario sobre redes, escolha uma alternativa e veja qual está correta ou errada')
+
+print(' Vamos começar')
+
+print ('Qual é a principal função do IP na rede?')
+print ('a - Ajudar a identificar e endereçar dispositivos na rede')
+print ('b - Controlar a velocidade da conexão ')
+print ('c - Criar redes Wi-Fi seguras')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'a':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+
+print ('Qual é a principal diferença entre IPv4 e IPv6?')
+print ('a -IPv4 tem endereços mais longos que IPv6')
+print ('b -IPv4 e IPv6 têm o mesmo número de endereços')
+print ('c -IPv4 tem menos endereços disponíveis do que IPv6')
+
+resposta = input('digite uma das opções')
+
+
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
+
+print ('Qual é a função principal do DNS?')
+print ('a -Transformar URLs em endereços IP')
+print ('b -Traduzir nomes de domínio para endereços IP')
+print ('c -Controlar a velocidade da Internet')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'b':
+    print ('resposta certa')
+if resposta == 'c':
+    print ('resposta errada')
+if resposta == 'a':
+    print ('resposta errada')
+
+print ('Por que o cabeamento estruturado é vantajoso para redes de computadores')
+print ('a -Porque diminui a velocidade dos dados')
+print ('b -Porque aumenta o custo dos cabos')
+print ('c -Porque facilita a manutenção e a expansão da rede')
+
+resposta = input('digite uma das opções')
+
+if resposta == 'c':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'b':
+    print ('resposta errada')
+
+print ('O que é uma característica importante dos cabos de fibra óptica em comparação com os cabos de cobre')
+print ('a -Menos capacidade de transmissão e mais perda de sinal')
+print ('b -Mais velocidade de transmissão e menos perda de sinal')
+print ('c - Maior custo e menor durabilidade') 
+
+resposta = input('digite uma das opções')
+
+if resposta == 'c':
+    print ('resposta errada')
+if resposta == 'a':
+    print ('resposta certa')
+if resposta == 'b':
+    print ('resposta errada')
